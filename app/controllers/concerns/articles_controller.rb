d01bf3c82b71906bf6599bb37a60fd2d75545466
anas90zh <anas90zh@gmail.com>
2020-07-29 16:22:30 +0200

    29.07.2020

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -1,4 +1,5 @@
 class  ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy] 
 
     def index
         @articles = Article.all
@@ -22,13 +23,11 @@ class  ArticlesController < ApplicationController
 
     end
     def show
-        @article = Article.find(params[:id])
         
     end
 
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = " article is deleted"
         redirect_to articles_path
@@ -36,13 +35,11 @@ class  ArticlesController < ApplicationController
     end
 
     def edit 
-        @article = Article.find(params[:id])
 
     end
 
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Arricle was deleted"
             redirect_to articles_path
@@ -63,7 +60,10 @@ class  ArticlesController < ApplicationController
 
         end
 
-        
+        def set_article
+            @article = Article.find(params[:id])
+
+        end
 
 
 end
