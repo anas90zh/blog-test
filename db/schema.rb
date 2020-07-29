efeb9571afbfd2b7ef0e8489b3738f80168ad36c
anas90zh <anas90zh@gmail.com>
2020-07-29 20:19:25 +0200

    user val

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_150717) do
+ActiveRecord::Schema.define(version: 2020_07_29_161503) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "descrition"
     t.datetime "created_at"
     t.datetime "upldated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
